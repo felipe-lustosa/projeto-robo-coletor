baf52f0d435693f8aa44e3713b4269dcbd692424
felipe-lustosa <felipelustosaa6@gmail.com>
2026-08-29 08:56:18 -0300

    FEAT: implement ComandoColeta class with execution and undo logic

--- src/celular_robo/comandos.py
+++ src/celular_robo/comandos.py
@@ -7,3 +7,28 @@
 # TODO: implemente aqui. ComandoColeta(Comando): __init__(codinome, posicao,
 # quantidade), com .executar(robo) e .desfazer(robo) (remove o item da
 # bandeja, decrementa a contagem coletada).
+from celular_robo.comandos_base import Comando
+from celular_robo.robo import QuantidadeValida
+
+
+class ComandoColeta(Comando):
+    """Um item do pedido: codinome, posição, quantidade pedida.
+    .executar(robo) delega a navegação/coleta pra robo.estrategia (a
+    RotaColeta configurada no robô)."""
+
+    quantidade_coletada = QuantidadeValida()
+
+    def __init__(self, codinome, posicao, quantidade):
+        self.codinome = codinome
+        self.posicao = posicao
+        self.quantidade = quantidade
+        self.quantidade_coletada = 0
+
+    def executar(self, robo):
+        robo.estrategia.coletar(robo, self)
+
+    def desfazer(self, robo):
+        pass
+
+    def __repr__(self):
+        return f"ComandoColeta({self.codinome!r}, {self.posicao}, {self.quantidade})"
